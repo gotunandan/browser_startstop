bb55c0a25acf459372882ecdfc15658db23e7332
Nandan <nandan@trustyY510P.nandanvaidya.net>
2014-06-17 18:30:36 +0530

    Commiting changes specified

--- init.rb
+++ init.rb
@@ -1,9 +1,3 @@
 require 'sinatra'
-#require 'sinatra/reloader' if development?
-
-$keys_time = {}
-$keys_list = {}
-$keys_list['available'] = []
-$keys_list['blocked'] = []
 
 require './browseractions'
